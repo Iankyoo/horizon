464cff92b53e8e3acd5620bfbafcc525b2532435
Iankyoo <contato.iankyoo@gmail.com>
2026-08-01 19:44:18 -0300

    Adiciona VagaRepository

---
+++ backend/src/main/java/com/iankyoo/horizon/repository/VagaRepository.java
@@ -0,0 +1,7 @@
+package com.iankyoo.horizon.repository;
+
+import com.iankyoo.horizon.model.Vaga;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VagaRepository extends JpaRepository<Vaga, Long> {
+}
