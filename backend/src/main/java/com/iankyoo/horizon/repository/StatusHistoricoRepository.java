0e14c44e61e279f9fc8eafd7f7ade9ab4373ab8d
Iankyoo <contato.iankyoo@gmail.com>
2026-08-01 19:45:55 -0300

    Adiciona StatusHistoricoRepository

---
+++ backend/src/main/java/com/iankyoo/horizon/repository/StatusHistoricoRepository.java
@@ -0,0 +1,7 @@
+package com.iankyoo.horizon.repository;
+
+import com.iankyoo.horizon.model.StatusHistorico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StatusHistoricoRepository extends JpaRepository<StatusHistorico, Long> {
+}
