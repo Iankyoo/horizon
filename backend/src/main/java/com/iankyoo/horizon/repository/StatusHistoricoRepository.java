02466c1d5f52725ae0434370bb78f2255814a884
Iankyoo <contato.iankyoo@gmail.com>
2026-08-01 20:43:31 -0300

    Adiciona findByVagaIdOrderByDataMudancaAsc em StatusHistoricoRepository

--- backend/src/main/java/com/iankyoo/horizon/repository/StatusHistoricoRepository.java
+++ backend/src/main/java/com/iankyoo/horizon/repository/StatusHistoricoRepository.java
@@ -3,5 +3,10 @@ package com.iankyoo.horizon.repository;
 import com.iankyoo.horizon.model.StatusHistorico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StatusHistoricoRepository extends JpaRepository<StatusHistorico, Long> {
+
+    List<StatusHistorico> findByVagaIdOrderByDataMudancaAsc(Long vagaId);
+
 }
